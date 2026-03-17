3a591d6c276e0fa65655c9b648d9fafce5cd90e2
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-17 19:34:11 +0100

    MILESTONE2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 
 function App() {
-  
+  const [autori, setAutori] = useState([])
 
   const linkApi = "https://lanciweb.github.io/demo/api/actors/"
 
@@ -12,18 +12,42 @@ function App() {
     fetch(linkApi)
     .then(res => res.json())
     .then(data => {
-      data.forEach(autore =>{
-        console.log(autore.name)
-      })
-
+      setAutori(data)
     })
   },[])
 
 
   return (
     <>
-    
+    <div className='section'>
+      <div className='container'>
+        <div className='row'>
+          {autori.map(autore => (
+            <div className='col-6' key={autore.id}> 
+              <div className='card'>
+                <img src={autore.image} className="card-img-top p-5"/>
+                <div className='card-body'>
+                  <p className='card-text'>{autore.name}</p>
+                  <p className='card-text'>Nato il: {autore.birth_year}</p>
+                  <p className='card-text'>Nazionalita: {autore.nationality}</p>
+                  <p className='card-text'>Biografia: {autore.biography}</p>
+                  <p className='card-text'>Riconoscimenti: {autore.awards}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+
+
+
+
     </>
+
+
+
   )
 }
 
