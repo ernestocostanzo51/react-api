5e60b9616456beb13f8e8e28c291f0698cdc9e73
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-17 20:05:42 +0100

    MILESTONE 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 
 
 
+
 function App() {
   const [autori, setAutori] = useState([])
 
@@ -19,15 +20,31 @@ function App() {
 
   return (
     <>
-    <div className='section'>
+    <div className='section pb-4 bg-dark'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+                <nav class="navbar navbar-expand-sm navbar-light">
+                            <div class="navbar-nav">
+                                <a class="nav-link active" aria-current="page" href="#">Autori Uomo</a> 
+                                <a class="nav-link active" aria-current="page" href="#">Autori Donna</a> 
+                            </div>
+                </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div className='section bg-secondary'>
       <div className='container'>
         <div className='row'>
           {autori.map(autore => (
-            <div className='col-6' key={autore.id}> 
-              <div className='card'>
+            <div className='col-6 mb-3 mt-2' key={autore.id}> 
+              <div className='card bg-body'>
                 <img src={autore.image} className="card-img-top p-5"/>
                 <div className='card-body'>
-                  <p className='card-text'>{autore.name}</p>
+                  <h5 className='card-text'>{autore.name}</h5>
                   <p className='card-text'>Nato il: {autore.birth_year}</p>
                   <p className='card-text'>Nazionalita: {autore.nationality}</p>
                   <p className='card-text'>Biografia: {autore.biography}</p>
