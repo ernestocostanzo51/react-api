d119e15578b748e88b9b7101b677aa535292dbda
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-03-17 19:11:45 +0100

    MILESTONE 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,21 @@ import { useEffect } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const linkApi = "https://lanciweb.github.io/demo/api/actors/"
+
+  useEffect(() =>{
+    fetch(linkApi)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(autore =>{
+        console.log(autore.name)
+      })
+
+    })
+  },[])
+
 
   return (
     <>
